49b7388a910ff9dec0cc3221794fd1915271e694
MOON <moonjun210@naver.com>
2024-03-25 14:40:19 +0900

    Crosshair update
    if push tab btn disappear crosshair

--- GunGokBaeProject/Assets/M_BagAndBoss/Scripts/M_PlayerMiningUI.cs
+++ GunGokBaeProject/Assets/M_BagAndBoss/Scripts/M_PlayerMiningUI.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class M_PlayerMiningUI : MonoBehaviour
 {
  
 
     private GameObject uiManager;
+    public Image crosshair;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +23,15 @@ public class M_PlayerMiningUI : MonoBehaviour
         if (Input.GetKey(KeyCode.Tab))
         {
             uiManager.SetActive(true);
+            crosshair.gameObject.SetActive(false);
         }
         else
         {
             uiManager.SetActive(false);
+            crosshair.gameObject.SetActive(true) ;
         }
 
-       
+
     }
 
 
