387b0df3ce62dab62aec37511c56a6265b90683a
heewon21 <162409451+HeeWon21@users.noreply.github.com>
2024-04-03 22:16:24 +0900

    아파치인지 PHP인지 연결

---
+++ GunGokBaeProject/Assets/Han/Han_Scripts/UnityWebRequestExample.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UnityWebRequestExample : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Ready1");
+        StartCoroutine("LoadFromPhp");
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator LoadFromPhp()
+    {
+        string url = "C:/xampp/htdocs/index.php";
+        WWW www = new WWW(url);
+        Debug.Log("Ready2");
+        yield return www;
+
+        if (www.isDone)
+        {
+            if (www.error == null)
+            {
+                Debug.Log("Receive Data : " + www.text);
+            }
+            else
+            {
+                Debug.Log("error : " + www.error);
+            }
+        }
+        Debug.Log("Ready3");
+    }
+}
+
+
